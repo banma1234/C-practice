991757ebbf2c327c45a2f1dc6871e4893c87c0eb
banma1234 <77619465+banma1234@users.noreply.github.com>
2021-01-21 20:35:41 +0900

    백준 9-4
    소수 구하기

---
+++ eratostenes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int M, N, arr[1000001] = { 0, };
+    scanf("%d %d", &M, &N);
+
+    arr[0] = 1, arr[1] = 1;
+    for (int j = 2; j < 1000001 / j; j++)
+    {
+        if (arr[j] == 1) continue;
+        for (int i = j * j; i < 1000001; i += j)
+            if (i % j == 0) arr[i] = 1;
+    }
+
+    for (int i = M; i <= N; i++)
+        if (arr[i] == 0)
+            printf("%d\n", i);
+    return 0;
+}
+
+// 20.01.21. 백준 1921 '소수 구하기'
